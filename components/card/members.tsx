b5cd6413bf08dc6b53c9c9dee1d461cf060feb89
huyhunhngc <huyhunhngc@gmail.com>
2024-06-22 23:52:29 +0700

    remove even group

--- components/card/members.tsx
+++ components/card/members.tsx
@@ -31,8 +31,8 @@ export default function Member({
       }}
       className="group mb-3 sm:mb-8 last:mb-0"
     >
-      <section className="flex flex-row items-center bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition sm:group-even:pl-8 dark:text-white dark:bg-white/10 dark:hover:bg-white/20">
-        <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[70%] flex flex-col h-full sm:group-even:ml-[10rem]">
+      <section className="flex flex-row items-center bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition dark:text-white dark:bg-white/10 dark:hover:bg-white/20">
+        <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[70%] flex flex-col h-full">
           <h3 className="text-2xl font-semibold">{title}</h3>
           <div className="block sm:hidden">
             <Image
@@ -77,7 +77,7 @@ export default function Member({
           object-cover
           transition-all
           hover:scale-[1.15]
-          group-odd:right-[initial] group-odd:right-11
+          right-10
           rounded-full ring-2 ring-gray-300 dark:ring-gray-500 my-auto"
           src={imageUrl}
           alt="Bordered avatar"
